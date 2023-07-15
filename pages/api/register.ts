ace01d77eee013607fc143941f6d698e09641797
nevesw <wneves@outlook.com>
2023-07-15 15:51:22 -0300

    ajuste api cadastro

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -1,35 +1,36 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { db } from "@/lib/db";
-import { createJWT, hashPassword } from "@/lib/auth";
-import { serialize } from "cookie";
+import { createJWT, hashPassword } from '@/lib/auth'
+import { db } from '@/lib/db'
+import {NextApiRequest, NextApiResponse} from 'next'
+import {serialize} from 'cookie'
 
-export default async function register(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method === "POST") {
+export default async function register(req: NextApiRequest, res: NextApiResponse) {
+
+
+  if (req.method === 'POST') {
     const user = await db.user.create({
       data: {
         email: req.body.email,
         password: await hashPassword(req.body.password),
         firstName: req.body.firstName,
-        lastName: req.body.lastName,
-      },
-    });
+        lastName: req.body.lastName
+      }
+    })
 
-    const jwt = await createJWT(user);
+    const jwt = await createJWT(user)
+
     res.setHeader(
-      "Set-Cookie",
-      serialize(process.env.COOKIE_NAME ? process.env.COOKIE_NAME : "", jwt, {
+      'Set-Cookie', 
+      serialize(process.env.COOKIE_NAME, jwt, {
         httpOnly: true,
         path: "/",
         maxAge: 60 * 60 * 24 * 7,
       })
-    );
-    res.status(201);
-    res.end();
+    )
+
+    res.status(201)
+    res.json({})
   } else {
     res.status(402);
-    res.end();
+    res.json({});
   }
 }
