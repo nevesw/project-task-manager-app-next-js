882ef0626f054ba9f90260644d942014ceca1ff7
nevesw <wneves@outlook.com>
2023-07-15 15:03:50 -0300

    componente criar novos projetos

--- lib/api.ts
+++ lib/api.ts
@@ -1,36 +1,35 @@
-const fetcher = async ({url, method, body, json = true}) => {
-    const res = await fetch(url, {
-        method,
-        ...(body && {body: JSON.stringify(body)}),
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json'
-        }
-    })
+const fetcher = async ({ url, method, body, json = true }) => {
+  const res = await fetch(url, {
+    method,
+    ...(body && { body: JSON.stringify(body) }),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
 
-    if (!res.ok) {
-        throw new Error(res.statusText)
-    }
+  if (!res.ok) {
+    throw new Error(res.statusText);
+  }
 
-    if(json){
-        const data = await res.json()
-        return data.data
-    }
-}
-
+  if (json) {
+    const data = await res.json();
+    return data.data;
+  }
+};
 
 export const register = (user) => {
-    return fetcher({url: '/api/register', method: 'POST', body: user})
-}
+  return fetcher({ url: "/api/register", method: "POST", body: user });
+};
 
 export const signin = (user) => {
-    return fetcher({url: '/api/signin', method: 'POST', body: user})
-}
+  return fetcher({ url: "/api/signin", method: "POST", body: user });
+};
 
 export const createNewProject = (name) => {
-    return fetcher({
-      url: "/api/project",
-      method: "POST",
-      body: { name },
-    });
-  };
+  return fetcher({
+    url: "/api/project",
+    method: "POST",
+    body: { name },
+  });
+};
